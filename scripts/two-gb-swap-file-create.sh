4b74427f048aab7585492763f0da8bfc9fc4de37
dvosburg <don@vosburgs.org>
2023-04-17 15:09:22 -0400

    Create two-gb-swap-file-create.sh

---
+++ scripts/two-gb-swap-file-create.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+dd if=/dev/zero of=/swapfile bs=1M count=2048
+chmod 0600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile                                  swap              swap  defaults  0  0' >> /etc/fstab
