b7477ef978c3426bcc6b045d3a10c62772d963dd
Don Vosburg <don@vosburgs.org>
2024-05-03 16:43:45 -0400

    Update get-suma-script.sh

--- suma5/get-suma-script.sh
+++ suma5/get-suma-script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -Sks https://github.com/dvosburg/susemanager-contributions/raw/master/suma5/suma5-bigdisk.sh > /usr/local/bin/suma5-bigdisk.sh
+curl -Sks https://raw.githubusercontent.com/dvosburg/susemanager-contributions/master/suma5/suma5-bigdisk.sh > /usr/local/bin/suma5-bigdisk.sh
 chmod +x /usr/local/bin/suma5-bigdisk.sh
