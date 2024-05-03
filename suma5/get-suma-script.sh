d7090ec8e9764051f596c746c67ba71a2f9fe970
Don Vosburg <don@vosburgs.org>
2024-05-03 16:36:28 -0400

    Create get-suma-script.sh

---
+++ suma5/get-suma-script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -Sks https://raw.githubusercontent.com/dvosburg/susemanager-contributions/master/scripts/machine-id-refresh.sh > /usr/local/bin/machine-id-refresh.sh
+chmod +x /usr/local/bin/*.sh
