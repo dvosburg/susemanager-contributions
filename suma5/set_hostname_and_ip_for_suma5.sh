05004b9bcda95a447160fce9d750182f243b4c7c
Don Vosburg <don@vosburgs.org>
2024-06-05 23:44:31 -0400

    Create set_hostname_and_ip_for_suma5.sh

---
+++ suma5/set_hostname_and_ip_for_suma5.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+hostnamectl set-hostname suma5-server-image.site.com
+echo "Hostname set to suma5-server-image.site.com"
+nmcli device modify eth0 \
+  ipv4.method manual \
+  ipv4.addresses 10.73.1.50/24 \
+  ipv4.gateway 10.173.1.1 \
+  ipv4.dns 10.173.1.1
+
+echo "IP address set to 10.173.1.50"
+echo  "rebooting in one minute..."
+shutdown -r 1
