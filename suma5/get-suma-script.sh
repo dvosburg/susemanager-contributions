4b5b671674dc9bac2a871e6d22e4ed58307a5cff
Don Vosburg <don@vosburgs.org>
2024-06-05 23:46:58 -0400

    Update get-suma-script.sh

--- suma5/get-suma-script.sh
+++ suma5/get-suma-script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 curl -Sks https://raw.githubusercontent.com/dvosburg/susemanager-contributions/master/suma5/suma5-bigdisk.sh > /usr/local/bin/suma5-bigdisk.sh
+curl -Sks https://raw.githubusercontent.com/dvosburg/susemanager-contributions/master/suma5/set_hostname_and_ip_for_suma5.sh > /usr/local/bin/set_hostname_and_ip_for_suma5.sh
 chmod +x /usr/local/bin/suma5-bigdisk.sh
+chmod +x /usr/local/bin/set_hostname_and_ip_for_suma5.sh
