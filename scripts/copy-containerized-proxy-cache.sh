051d0096b16ea11102c18d277bd003dea3237beb
Don Vosburg <don@vosburgs.org>
2024-11-22 21:57:47 -0500

    Update copy-containerized-proxy-cache.sh

--- scripts/copy-containerized-proxy-cache.sh
+++ scripts/copy-containerized-proxy-cache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #  This script copies the proxy data from an existing containerized proxy to this one
-# Define the variable
+# Define the source proxy server variable
 PROXY1=suma5-proxy1.site.com
 # Start by ensuring the services are stopped
 mgrpxy stop
@@ -8,11 +8,11 @@ mgrpxy stop
 mkdir -p /var/lib/containers/storage/volumes/backupvolumes/
 mv /var/lib/containers/storage/volumes/uy* /var/lib/containers/storage/volumes/backupvolumes/
 #
-# Copy the files from server1 to this proxy with rsync
+# Copy the files from PROXY1 to this proxy with rsync
 rsync -Pav root@$PROXY1:/var/lib/containers/storage/volumes/uy* /var/lib/containers/storage/volumes/
 echo "Copying of proxy cache complete!"
 #
-# Start up the proxy on this server
+# Start up the proxy services on this server
 mgrpxy start
 #
 echo "Proxy startup complete with new cache"
