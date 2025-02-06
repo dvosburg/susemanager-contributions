b222932681600bdbb2a4e275545acbfdac76f370
Don Vosburg <don@vosburgs.org>
2025-02-06 16:02:30 -0500

    add repo removal to clean process

--- scripts/prep-and-clean.sh
+++ scripts/prep-and-clean.sh
@@ -4,8 +4,10 @@
 if [ -f "/usr/bin/zypper" ] && [ -d "/etc/venv-salt-minion" ]; then
 DIRECTORY="venv-salt-minion"
 zypper -n rm venv*
+rm /etc/zypp/repos.d/suseman*
 elif [ -d "/etc/yum.repos.d" ] && [ -d "/etc/venv-salt-minion" ]; then
 yum -y erase venv*
+rm -rf /etc/yum.repos.d/suseman*
 DIRECTORY="venv-salt-minion"
 elif [ -f "/usr/bin/apt" ] && [ -d "/etc/venv-salt-minion" ]; then
 DIRECTORY="venv-salt-minion"
@@ -17,8 +19,10 @@ if [ -f "/usr/bin/apt" ] && [ -d "/etc/salt" ]; then
 apt remove -y salt-minion
 elif [ -d "/etc/yum.repos.d" ] && [ -d "/etc/salt" ]; then
 yum -y erase salt-minion
+rm -rf /etc/yum.repos.d/suseman*
 elif [ -f "/usr/bin/zypper" ] && [ -d "/etc/salt" ]; then
 zypper -n rm salt-minion
+rm /etc/zypp/repos.d/suseman*
 fi
 rm /var/cache/"$DIRECTORY"/* -Rf
 rm /etc/"$DIRECTORY"/minion_id -Rf
